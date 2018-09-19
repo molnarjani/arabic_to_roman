6b60737070103e1f5147d665821d9b22f7a90506
yonash <janosmolnar@hu.ibm.com>
2018-09-19 14:56:37 +0200

    Added test for supported range

--- roman.py
+++ roman.py
@@ -1,4 +1,5 @@
 class RomanNumber(str):
     @classmethod
     def from_arabic(cls, num):
-        pass
+        if not 0 < num < 4000:
+            raise ValueError("Cannot represent number in Roman")
