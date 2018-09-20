929750d013b01a70a7502871f9d11e00771cbfb5
yonash <janosmolnar@hu.ibm.com>
2018-09-20 12:37:55 +0200

    Implemented arabic to roman conversion algorithm

--- test_roman.py
+++ test_roman.py
@@ -13,8 +13,22 @@ def test_overflow():
     with pytest.raises(ValueError):
         RomanNumber.from_arabic(4000)
 
-literals = ('I', 'V', 'X', 'L', 'C', 'D', 'M')
-numbers = (1, 5, 10, 50, 100, 500, 1000)
-@pytest.mark.parametrize('arabic,roman', zip(numbers, literals))
+arabic = (1, 5, 10, 50, 100, 500, 1000)
+roman = ('I', 'V', 'X', 'L', 'C', 'D', 'M')
+@pytest.mark.parametrize('arabic,roman', zip(arabic, roman))
 def test_basic_literal(arabic, roman):
-    RomanNumber.from_arabic(1) == 'i'
+    assert RomanNumber.from_arabic(arabic) == roman
+
+def test_roman_addition():
+    assert RomanNumber.from_arabic(3) == 'III'
+
+def test_roman_subtraction():
+    assert RomanNumber.from_arabic(4) == 'IV'
+
+def test_roman_examples():
+    assert RomanNumber.from_arabic(2417) == 'MMCDXVII'
+    assert RomanNumber.from_arabic(999) == 'CMXCIX'
+    assert RomanNumber.from_arabic(290) == 'CCXC'
+    assert RomanNumber.from_arabic(1742) == 'MDCCXLII'
+    assert RomanNumber.from_arabic(3974) == 'MMMCMLXXIV'
+
